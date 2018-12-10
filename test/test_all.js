c69daa37251b49647561ca8f45a158ac2273726f
austil <augustin@mrlacour.fr>
2018-12-10 18:38:26 +0100

    Renamed feature aggre with proper name

--- test/test_all.js
+++ test/test_all.js
@@ -1,12 +1,12 @@
 const {genTest, runTest} = require("./test_utils");
 
 // PEAK - FEATURE
-genTest("peak", "feature_after_1");
-runTest("peak", "feature_after_1", [0,3,5,6,2,0,0], "width max", 4);
-runTest("peak", "feature_after_1", [7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1], "width max", 3);
-runTest("peak", "feature_after_1", [0,3,5,6,2,0,0], "max max", 6);
-runTest("peak", "feature_after_1", [0,3,5,6,2,0,0], "min min", 2);
-runTest("peak", "feature_after_1", [0,3,5,6,2,0,0], "surface max", 16);
+genTest("peak", "feature_aggr_after_1");
+runTest("peak", "feature_aggr_after_1", [0,3,5,6,2,0,0], "width max", 4);
+runTest("peak", "feature_aggr_after_1", [7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1], "width max", 3);
+runTest("peak", "feature_aggr_after_1", [0,3,5,6,2,0,0], "max max", 6);
+runTest("peak", "feature_aggr_after_1", [0,3,5,6,2,0,0], "min min", 2);
+runTest("peak", "feature_aggr_after_1", [0,3,5,6,2,0,0], "surface max", 16);
 
 // PEAK - FOOTPRINT
 genTest("peak", "footprint");
@@ -16,12 +16,12 @@ runTest("peak", "footprint",
 );
 
 // SUMMIT - FEATURE
-genTest("summit", "feature_after_1");
-runTest("summit", "feature_after_1", [0,1,1,2,1,1,0], "width max", 5);
-runTest("summit", "feature_after_1", [0,1,1,2,1,1,0], "max max", 2);
-runTest("summit", "feature_after_1", [0,1,1,2,1,1,0], "min min", 1);
-runTest("summit", "feature_after_1", [0,1,1,2,1,1,0], "surface max", 6);
-runTest("summit", "feature_after_1", [7,1,5,4,4,3,3,4,6,6,2,3,4,2,3,1], "surface max", 13);
+genTest("summit", "feature_aggr_after_1");
+runTest("summit", "feature_aggr_after_1", [0,1,1,2,1,1,0], "width max", 5);
+runTest("summit", "feature_aggr_after_1", [0,1,1,2,1,1,0], "max max", 2);
+runTest("summit", "feature_aggr_after_1", [0,1,1,2,1,1,0], "min min", 1);
+runTest("summit", "feature_aggr_after_1", [0,1,1,2,1,1,0], "surface max", 6);
+runTest("summit", "feature_aggr_after_1", [7,1,5,4,4,3,3,4,6,6,2,3,4,2,3,1], "surface max", 13);
 
 // SUMMIT - FOOTPRINT
 genTest("summit", "footprint");
@@ -31,11 +31,11 @@ runTest("summit", "footprint",
 );
 
 // PROPER PLATEAU - FEATURE
-genTest("proper_plateau", "feature_after_1");
-runTest("proper_plateau", "feature_after_1", [0,1,1,1,0,0,0], "width max", 3);
-runTest("proper_plateau", "feature_after_1", [0,1,1,1,0,0,0], "max max", 1);
-runTest("proper_plateau", "feature_after_1", [0,1,1,1,0,0,0], "min min", 1);
-runTest("proper_plateau", "feature_after_1", [0,1,1,1,0,0,0], "surface max", 3);
+genTest("proper_plateau", "feature_aggr_after_1");
+runTest("proper_plateau", "feature_aggr_after_1", [0,1,1,1,0,0,0], "width max", 3);
+runTest("proper_plateau", "feature_aggr_after_1", [0,1,1,1,0,0,0], "max max", 1);
+runTest("proper_plateau", "feature_aggr_after_1", [0,1,1,1,0,0,0], "min min", 1);
+runTest("proper_plateau", "feature_aggr_after_1", [0,1,1,1,0,0,0], "surface max", 3);
 
 // PROPER PLATEAU - FOOTPRINT
 genTest("proper_plateau", "footprint");
@@ -45,13 +45,13 @@ runTest("proper_plateau", "footprint",
 );
 
 // INFLEXION - FEATURE
-genTest("inflexion", "feature_after_1");
-runTest("inflexion", "feature_after_1", [1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4], "width max", 3);
-runTest("inflexion", "feature_after_1", [0,1,2,3,2,0,0], "width max", 3);
-runTest("inflexion", "feature_after_1", [0,1,2,3,2,0,0], "max max", 3);
-runTest("inflexion", "feature_after_1", [0,1,2,3,2,0,0], "min min", 1);
-runTest("inflexion", "feature_after_1", [1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4], "surface max", 14);
-runTest("inflexion", "feature_after_1", [0,1,2,3,2,0,0], "surface max", 6);
+genTest("inflexion", "feature_aggr_after_1");
+runTest("inflexion", "feature_aggr_after_1", [1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4], "width max", 3);
+runTest("inflexion", "feature_aggr_after_1", [0,1,2,3,2,0,0], "width max", 3);
+runTest("inflexion", "feature_aggr_after_1", [0,1,2,3,2,0,0], "max max", 3);
+runTest("inflexion", "feature_aggr_after_1", [0,1,2,3,2,0,0], "min min", 1);
+runTest("inflexion", "feature_aggr_after_1", [1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4], "surface max", 14);
+runTest("inflexion", "feature_aggr_after_1", [0,1,2,3,2,0,0], "surface max", 6);
 
 // INFLEXION - FOOTPRINT
 genTest("inflexion", "footprint");
@@ -61,10 +61,10 @@ runTest("inflexion", "footprint",
 );
 
 // DESCREASING - FEATURE
-genTest("decreasing", "feature_after_0");
-runTest("decreasing", "feature_after_0", [3,4,4,3,2,2,1], "width max", 2);
-runTest("decreasing", "feature_after_0", [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4], "surface max", 10);
-runTest("decreasing", "feature_after_0", [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4], "min min", 1);
+genTest("decreasing", "feature_aggr_after_0");
+runTest("decreasing", "feature_aggr_after_0", [3,4,4,3,2,2,1], "width max", 2);
+runTest("decreasing", "feature_aggr_after_0", [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4], "surface max", 10);
+runTest("decreasing", "feature_aggr_after_0", [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4], "min min", 1);
 
 // DESCREASING - FOOTPRINT
 genTest("decreasing", "footprint");
